aaee98a95d521f6097a4168dd259bb827131ab72
AdrianChabrera <adrianchabrera@gmail.com>
2026-03-21 14:25:48 +0100

    feat: add power supply entity and mapper

---
+++ backend/src/components/entities/main-entities/power-supply.entity.ts
@@ -0,0 +1,57 @@
+import { Column, Entity } from 'typeorm';
+import { Component } from '../component.entity';
+
+@Entity('power_supplies')
+export class PowerSupply extends Component {
+  @Column({ type: 'int', nullable: true })
+  wattage: number | null;
+
+  @Column({ type: 'int', nullable: true, name: 'atx_24_pin' })
+  atx24Pin: number | null;
+
+  @Column({ type: 'int', nullable: true, name: 'eps_8_pin' })
+  eps8Pin: number | null;
+
+  @Column({ type: 'int', nullable: true, name: 'pcie_12vhpwr' })
+  pcie12Vhpwr: number | null;
+
+  @Column({ type: 'int', nullable: true, name: 'pcie_6_plus_2_pin' })
+  pcie6Plus2Pin: number | null;
+
+  @Column({ type: 'int', nullable: true })
+  sata: number | null;
+
+  @Column({
+    type: 'decimal',
+    precision: 5,
+    scale: 2,
+    nullable: true,
+  })
+  length: number | null;
+
+  @Column({ type: 'boolean', nullable: true })
+  fanless: boolean | null;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    nullable: true,
+    name: 'form_factor',
+  })
+  formFactor: string | null;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    nullable: true,
+    name: 'efficency_rating',
+  })
+  efficiencyRating: string | null;
+
+  @Column({
+    type: 'varchar',
+    length: 100,
+    nullable: true,
+  })
+  modular: string | null;
+}
